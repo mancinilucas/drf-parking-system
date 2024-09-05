171b391c140905299869a79ee50c019b6f343af9
mancinilucas <mancini.lucasg@gmail.com>
2024-09-04 22:40:08 -0300

    add: super user

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Commercial_Establishment, Vehicle
+
+admin.site.register(Commercial_Establishment)
+admin.site.register(Vehicle)
