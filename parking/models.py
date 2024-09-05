685dab47df566e7ec25dad004f5c58701a186e17
mancinilucas <mancini.lucasg@gmail.com>
2024-09-04 22:28:04 -0300

    add: first migration

--- parking/models.py
+++ parking/models.py
@@ -5,10 +5,9 @@ class Commercial_Establishment(models.Model):
     name = models.CharField(max_length=100, blank=False)
     cnpj = models.CharField(max_length=14, blank=False)
     adress = models.CharField(max_length=100, blank=False)
-    phone = models.IntegerField(max_length=14, blank=False)
-    car_parking_spaces = models.IntegerField(max_length=100, blank=False)
-    motorcycle_parking_spaces = models.IntegerField(
-        max_length=100, blank=False)
+    phone = models.IntegerField(blank=False)
+    car_parking_spaces = models.IntegerField(blank=False)
+    motorcycle_parking_spaces = models.IntegerField(blank=False)
 
     def __str__(self):
         return f'Nome do estabelecimento: {self.name} | CNPJ: {self.cnpj}'
@@ -18,7 +17,7 @@ class Vehicle(models.Model):
     brand = models.CharField(max_length=50, blank=False)
     model = models.CharField(max_length=50, blank=False)
     color = models.CharField(max_length=20, blank=False)
-    car_plate = models.CharField(max_length=7, blank=False)
+    car_plate = models.CharField(max_length=7, blank=False, unique=True)
     type = models.CharField(max_length=20, blank=False)
 
     def __str__(self):
