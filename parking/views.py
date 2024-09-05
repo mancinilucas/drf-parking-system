26966f837aa15eb8eb53671c1c6fa837c1b74067
mancinilucas <mancini.lucasg@gmail.com>
2024-09-04 22:56:00 -0300

    add: parking views

--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Commercial_Establishment, Vehicle
+from .serializers import CommercialEstablishmentSerializer, VehicleSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_all_establishments(request):
+    if request.method == 'GET':
+        establishments = Commercial_Establishment.objects.all()
+        serializer = CommercialEstablishmentSerializer(
+            establishments, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
